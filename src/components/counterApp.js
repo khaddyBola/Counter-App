43d2a7e978bb08573a11d3e11a46c685a371835e
user <user@users-MacBook-Air.local>
2022-07-03 13:38:46 +0100

    First commit

---
+++ src/components/counterApp.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+class CounterApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this)
+        this.handleMinusOne = this.handleMinusOne.bind(this)
+        this.handleReset = this.handleReset.bind(this)
+
+
+        this.state = {
+            appOwner: 'Khaddy Bola...App 👍😎',
+            counter: 0
+        }
+    }
+
+    handleAddOne() {
+        // State Component
+
+        this.setState((prevState) => {
+            return {
+                counter: prevState.counter + 1
+            }
+        })
+    }
+
+    handleMinusOne() {
+        this.setState((prevState) => {
+            if (this.state.counter >= 1) {
+                return {
+                    counter: prevState.counter -1
+                }
+            }
+        
+        })
+    }
+
+    handleReset() {
+        this.setState(() => {
+            return {
+                counter:  0
+            }
+        })
+    }
+    render() {
+        return (
+        <div>
+            <div className="header">
+                <div className="container">
+                    <h2 className="header-name">{this.state.appOwner}</h2>
+                    <h1 className="header-title">counter: {this.state.counter}</h1>
+                    <div className="btn">
+                        <button onClick={this.handleAddOne}>AddOne +1</button>
+                        <button onClick={this.handleMinusOne}>MinusOne -1</button>
+                        <button onClick={this.handleReset}>Reset</button>
+                    </div>
+                    
+                </div>
+                
+            </div>
+           
+        </div>)
+    }
+
+
+}
+export default CounterApp;
